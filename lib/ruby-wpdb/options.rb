5531ba1e33fea8f450d3ede33523c75c0d2be0c0
Rob Miller <rob@bigfish.co.uk>
2013-09-06 12:03:05 +0100

    Add Option::get_option(name) as a shorthand for fetching options

--- lib/ruby-wpdb/options.rb
+++ lib/ruby-wpdb/options.rb
@@ -1,4 +1,12 @@
 module WPDB
   class Option < Sequel::Model(:"#{WPDB.prefix}options")
+    # Given the name of an option, will return the option value for that
+    # option - or nil of no option with that name exists.
+    #
+    # @param [String] name The option_name to fetch
+    # @return String
+    def self.get_option(name)
+      Option.where(:option_name => name).get(:option_value)
+    end
   end
 end
