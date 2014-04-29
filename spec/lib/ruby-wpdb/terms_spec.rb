483e746bd8cfa69b468846c365ceb27c2263db6b
Rob Miller <rob@bigfish.co.uk>
2014-04-29 14:21:04 +0100

    Better coverage for terms tests

--- spec/lib/ruby-wpdb/terms_spec.rb
+++ spec/lib/ruby-wpdb/terms_spec.rb
@@ -32,5 +32,16 @@ module WPDB
       post.termtaxonomy.first.term_id.should == @term.term_id
       post.termtaxonomy.first.taxonomy.should == 'category'
     end
+
+    it "attaches terms to posts by ID" do
+      post = Post.create(:post_title => 'test', :post_author => 1)
+      post.add_term(@term.term_id, 'category')
+      post.save
+
+      post.ID.should be > 0
+      post.termtaxonomy.length.should be > 0
+      post.termtaxonomy.first.term_id.should == @term.term_id
+      post.termtaxonomy.first.taxonomy.should == 'category'
+    end
   end
 end
