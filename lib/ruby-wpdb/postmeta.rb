d6520ec13d438b73a44dc4bbea9bf66d9128e712
Rob Miller <rob@bigfish.co.uk>
2015-04-15 15:09:45 +0100

    Remove extraneous comment

--- lib/ruby-wpdb/postmeta.rb
+++ lib/ruby-wpdb/postmeta.rb
@@ -1,6 +1,5 @@
 module WPDB
   class PostMeta < Sequel::Model(:"#{WPDB.prefix}postmeta")
-    # Many postmeta belongs to ONE post, not to more tha one post
     many_to_one :post, class: 'WPDB::Post'
   end
 end
